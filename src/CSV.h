39a07a7f1e65c1315aa67db17fb11f85a2b9f9a2
Chance Nelson <cbn35@nau.edu>
2017-10-27 17:24:06 -0700

    Begin rework of the CSV parser. Header file includes likely needed includes, a #define for a buffer size, and an include guard.
    Also, added a submodule in /lib for PPM-Converter

---
+++ src/CSV.h
@@ -0,0 +1,20 @@
+/* CSV Parsing library for a raycaster
+ * CS-430
+ * Chance Nelson
+ */
+
+#ifndef H_CSV
+#define H_CSV
+
+#define BUFFER_SIZE 255
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3DMath.h"
+#include "CSV.c"
+
+Entity* process_csv(FILE *file);  // Process a full CSV file
+Entity  parse_line(char *line);   // Parse a line of a CSV file
+
+#endif
