d87c3aa598efbd5c6208761d1c67dca36dec718c
Chance Nelson <cbn35@nau.edu>
2017-10-27 18:43:14 -0700

    Removed debug print statement

--- src/Raytrace.c
+++ src/Raytrace.c
@@ -124,7 +124,6 @@ Color shoot(Ray v, Entity *entities, Color background, int entitiesLen, int phon
         if(phong != 1 && intersect[3] < t) {
             t = intersect[3];
             pixColor = entities[i].color;
-            //printf("%f\n", t);
         } else if(phong == 1 && intersect[3] < t) {
             // Calculate color based on Phong shading model
             // Use recursion and shit.
