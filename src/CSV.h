35bc0c50eaa40b29c049dd89a4c79141003a0829
Chance Nelson <cbn35@nau.edu>
2017-10-27 19:31:54 -0700

    Changed include to Raytrace.h

--- src/CSV.h
+++ src/CSV.h
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "3DMath.h"
+#include "Raytrace.h"
 #include "CSV.c"
 
 Entity* process_csv(FILE *file);  // Process a full CSV file
